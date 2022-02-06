f6e632375c84512fab0c052003288ac1ff495474
Dominic Humber <dominichumber@Dominics-MacBook-Air.local>
2022-02-06 12:26:22 -0500

    Assert the order the models create in

--- src/utils/model-create.js
+++ src/utils/model-create.js
@@ -1,9 +1,9 @@
-const { Pokemon, Spawn, Evolution } = require("./models-create");
+const { Pokemon, Spawn, Evolution } = require("./models");
 
-function createModels() {
-    Pokemon.sync({ force: true });
-    Spawn.sync({ force: true });
-    Evolution.sync({ force: true });
+async function createModels() {
+    await Pokemon.sync({ force: true });
+    await Spawn.sync({ force: true });
+    await Evolution.sync({ force: true });
 }
 
-module.exports = createModels
+createModels();
