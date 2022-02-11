34fb4af6b1c1c75f234d2525af66b7f429a4c6e8
Dominic Humber <dominichumber@Dominics-MacBook-Air.local>
2022-02-10 21:06:20 -0500

    Moved to src/database

--- src/utils/model-create.js
+++
@@ -1,9 +0,0 @@
-const { Pokemon, Spawn, Evolution } = require("./models");
-
-async function createModels() {
-    await Pokemon.sync({ force: true });
-    await Spawn.sync({ force: true });
-    await Evolution.sync({ force: true });
-}
-
-createModels();
